dfe73d5424647f303a6742701bb67d19e5ee876b
Jaz <jazlalli@hotmail.com>
2013-03-19 18:07:11 +0000

    ui enhancements

--- public/js/directives.js
+++ public/js/directives.js
@@ -41,6 +41,12 @@ angular.module('directives', [])
 
 			element.addClass(scope.card.CategoriesCss);
 			element.data('order', scope.card.DisplayOrder);
+			
+			element.hover(function () {
+				$(this).find('img').fadeTo(300, 0.1);
+			}, function() {
+			    $(this).find('img').fadeTo(300, 1);
+			});
 
 			if (scope.$last) {
 				scope.$emit('lastItemLoaded');
