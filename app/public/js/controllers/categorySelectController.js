4f1619149b822cd382a27ffbdaf61a35d4640438
jazlalli <jazlalli@hotmail.com>
2013-04-16 22:50:59 +0100

    adding category select form modal assets

--- app/public/js/controllers/categorySelectController.js
+++ app/public/js/controllers/categorySelectController.js
@@ -3,8 +3,6 @@ define(['app'], function ($app) {
 
 	return $app.controller('categorySelectController', ['$scope', '$routeParams', '$http' , function ($scope, $routeParams, $http) {
 		$scope.Test = 'Here is a test string';
-		$scope.balance = false;
-		console.log($scope.balance);
-
+		
 	}]);
 });
